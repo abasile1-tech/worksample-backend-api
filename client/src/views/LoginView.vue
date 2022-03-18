4684d83f044015d8300b776eef8427a1e8c829f3
John <ashton.basile@gmail.com>
2022-03-18 14:24:07 +0100

    I added a route for signup and another for login.

---
+++ client/src/views/LoginView.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="login">
+    <h1>Please enter your login information to sign in to your account.</h1>
+  </div>
+</template>
+
+<style>
+@media (min-width: 1024px) {
+  .login{
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
--- client/src/views/SignupView.vue
+++ client/src/views/SignupView.vue
@@ -1,12 +1,12 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div class="signup">
+    <h1>Please enter your information to create an account.</h1>
   </div>
 </template>
 
 <style>
 @media (min-width: 1024px) {
-  .about {
+  .signup{
     min-height: 100vh;
     display: flex;
     align-items: center;
